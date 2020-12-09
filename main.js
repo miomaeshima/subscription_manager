cc76787a4f9f51ec4b12be050a94ab5430657fad
miomaeshima <miomamomo@gmail.com>
2020-12-09 11:41:33 +0900

    Added post to add new data to database

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let container = document.getElementById("container");
 
 //server側のendpoint(ここではinfo）からデータをとる。
@@ -5,5 +7,35 @@ fetch("http://localhost:3000/info")
   //responseオブジェクトからJSONを取り出すにはjson()をする必要がある。
   .then((res) => res.json())
   .then((data) => {
-    data.forEach(item=>(container.innerHTML+=`<li>${item}</li>`))
+    data.forEach((item) => (container.innerHTML += `<li>${item}</li>`));
   });
+
+let addSubscription = document.getElementById("addSubscription");
+addSubscription.addEventListener("submit", addSubsc);
+
+function addSubsc(e) {
+  e.preventDefault();
+  let date = form.date.value;
+  let service_name = form.service_name.value;
+  let url = form.url.value;
+  let price = form.price.value;
+  let description = form.description.value;
+  console.log(typeof date);
+
+  fetch("http://localhost:3000/form", {
+    method: "POST",
+    headers: {
+      Accetpt: "application/json, text/plain, */*",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      date: date,
+      service_name: service_name,
+      url: url,
+      price: price,
+      description: description
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
--- server.js
+++ server.js
@@ -1,33 +1,56 @@
-//.envが読み込めるようにdotenvを入れる。
+//importing dotenv to readd from .env
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-//node-progress(pg)を入れる。
+//import node-progress(pg)
 const { Client } = require("pg");
 const app = express();
 
+//making extended as true, you can get the response in a form of an array(?)
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-//pgでclient instanceをつくる。これがdbの代わりをしてくれる。.envに入っているコネクションチェーンでdbにつながる。
-  const client = new Client({
+//Use pg to make client, which is connected to the db using the info in the .env file.
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
- 
 });
 
 client.connect();
 
-
 app.get("/info", async (req, res) => {
   let result = [];
   const data = await client.query("select*from current_subscriptions");
-//テーブルの列が配列になって、それがたくさん入った配列が返ってくる。
-  for(element of data.rows){
-    result.push(element.service_name)
+  //The response is an array of many arrays, each representing a row in the table.
+  for (element of data.rows) {
+    result.push(element.service_name);
   }
-    res.send(result);
-  await client.end()
+  res.send(result);
+  //await client.end(); ←This stops server??
 });
+
+//Defining the format of a prepared statement/parametarized query
+let text =
+  "insert into current_subscriptions (date, service_name, url, price_per_year, description) values ($1, $2, $3, $4, $5)";
+
+app.post("/form", async (req, res) => {
+  try {
+    const data = await client.query(text, [
+      req.body.date,
+      req.body.service_name,
+      req.body.url,
+      req.body.price,
+      req.body.description,
+    ]);
+    console.log(data);
+  } catch (err) {
+    console.log(err.stack);
+  }
+});
+
+// app.get("/", (req, res) => {
+//   res.sendFile("index.html");
+// });
 
 app.listen(3000);
